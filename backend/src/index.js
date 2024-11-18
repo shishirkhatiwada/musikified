dba357a4bd6406e372c7495c4b790f10bbd09b6b
shishirkhatiwada <khatiwadaccr@gmail.com>
2024-11-18 17:35:48 +0545

    added express file upload and set up the file upload middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
+import path from "path";
+
+const __dirname = path.resolve();
 
 dotenv.config({});
 app.use(express.json());
-app.use(clerkMiddleware());
+app.use(clerkMiddleware()); // this will add auth to req obj => we will get userId, and other things from clerk
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "temp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024,
+    },
+  })
+);
 
 import userRoutes from "./routes/user.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
