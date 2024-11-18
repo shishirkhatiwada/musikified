440a2712672505ce950159eaedbbff000bb62f2d
shishirkhatiwada <khatiwadaccr@gmail.com>
2024-11-18 11:15:42 +0545

    clerk middleware done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,29 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
 
-dotenv.config({})
-app.use(express.json())
+dotenv.config({});
+app.use(express.json());
+app.use(clerkMiddleware());
 
-import userRoutes from "./routes/user.routes.js"
-import adminRoutes from "./routes/admin.routes.js"
-import songRoutes from "./routes/songs.routes.js"
-import authRoutes from "./routes/auth.routes.js"
-import albumbRoutes from "./routes/album.routes.js"
-import connectDb from "./lib/db.js"
+import userRoutes from "./routes/user.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
+import songRoutes from "./routes/songs.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import albumbRoutes from "./routes/album.routes.js";
+import connectDb from "./lib/db.js";
 
-const app = express()
+const app = express();
 
-const PORT = 8080
+const PORT = 8080;
 
-app.use("/api/users", userRoutes)
-app.use("/api/admin", adminRoutes)
-app.use("/api/songs", songRoutes)
-app.use("/api/albumb", albumbRoutes)
-app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albumb", albumbRoutes);
+app.use("/api/auth", authRoutes);
 
-
-app.listen(PORT, ()=>{
-console.log(`listening on port ${PORT}`);
-connectDb()
-
-})
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+  connectDb();
+});
