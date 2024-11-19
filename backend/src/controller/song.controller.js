dcffbb1e5151f5d9d4fbc603a50dff6412ee3eaf
shishirkhatiwada <khatiwadaccr@gmail.com>
2024-11-19 20:24:49 +0545

    made all the songs related features

--- backend/src/controller/song.controller.js
+++ backend/src/controller/song.controller.js
@@ -1,4 +1,5 @@
 import { Song } from "../models/song.models.js";
+
 export const getAllSongs = async (req, res) => {
   try {
     const songs = await Song.find().sort({ createdAt: -1 });
@@ -12,12 +13,11 @@ export const getFeaturedSongs = async (req, res) => {
   try {
     const songs = await Song.aggregate([
       {
-        $sample: { size: 5 },
+        $sample: { size: 6 },
       },
       { $project: { _id: 1, title: 1, artist: 1, imageUrl: 1, audioUrl: 1 } },
-    ])
-    res.status(200).json(songs)
-    ;
+    ]);
+    res.status(200).json(songs);
   } catch (error) {
     console.log(error);
   }
@@ -25,7 +25,12 @@ export const getFeaturedSongs = async (req, res) => {
 
 export const getTrendingSongs = async (req, res) => {
   try {
-    const songs = await Song.find().sort({ views: -1 });
+    const songs = await Song.aggregate([
+      {
+        $sample: { size: 4 },
+      },
+      { $project: { _id: 1, title: 1, artist: 1, imageUrl: 1, audioUrl: 1 } },
+    ]);
     res.status(200).json(songs);
   } catch (error) {
     console.log(error);
@@ -33,10 +38,16 @@ export const getTrendingSongs = async (req, res) => {
 };
 
 export const getMadeForYouSongs = async (req, res) => {
-  try {
-    const songs = await Song.find().sort({ createdAt: -1 });
-    res.status(200).json(songs);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const songs = await Song.aggregate([
+          {
+            $sample: { size: 5 },
+          },
+          { $project: { _id: 1, title: 1, artist: 1, imageUrl: 1, audioUrl: 1 } },
+        ])
+        res.status(200).json(songs)
+        ;
+      } catch (error) {
+        console.log(error);
+      }
 };
