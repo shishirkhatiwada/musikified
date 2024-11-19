9dd95394506d0a5bc07dfd15e9135117086c4e86
shishirkhatiwada <khatiwadaccr@gmail.com>
2024-11-19 20:14:35 +0545

    made song routes

--- backend/src/routes/songs.routes.js
+++ backend/src/routes/songs.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { getAllSongs, getFeaturedSongs, getMadeForYouSongs, getTrendingSongs } from "../controller/song.controller.js";
+import { protectedRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const route = Router()
 
-route.get("/songs", (req,res)=>{
-    console.log(`This is a songs route`);
-    
-})
-
+route.get("/", protectedRoute, requireAdmin, getAllSongs)
+route.get("/featured", getFeaturedSongs )
+route.get("/made-for-you", getMadeForYouSongs)
+route.get("/trending", getTrendingSongs)
 
 export default route
