36636684a190cb5899eb7bb48a436f30a105986b
shishirkhatiwada <khatiwadaccr@gmail.com>
2024-11-22 14:06:04 +0545

    made homepage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
+import TopBar from "@/components/TopBar";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <TopBar />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
