98fe1ae52ff44a93c40e1ab0931b748e9bada28b
shishirkhatiwada <khatiwadaccr@gmail.com>
2024-11-22 14:04:55 +0545

    made changes in user models

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    fullname:{type:String, required:true},
-    imageUrl:{type:String, required:true},
-    clerkId:{type:String,required:true, uinque:true},
-}, {timestamps:true})
+const userSchema = new mongoose.Schema(
+    {
+        fullname: { type: String, required: true },
+        imageUrl: { type: String, required: true },
+        clerkId: { type: String, required: true, unique: true }, // Fixed typo
+    },
+    { timestamps: true }
+);
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
