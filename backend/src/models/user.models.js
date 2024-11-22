50412974753f6953834478e0fbb7c104d951bc67
shishirkhatiwada <khatiwadaccr@gmail.com>
2024-11-22 19:07:25 +0545

    had a typo on clerk , fixed it

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
     {
         fullname: { type: String, required: true },
         imageUrl: { type: String, required: true },
-        clerkId: { type: String, required: true, unique: true }, // Fixed typo
+        clerkId: { type: String, required: true, unique: true }, 
     },
     { timestamps: true }
 );
